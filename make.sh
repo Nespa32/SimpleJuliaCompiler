2460b695b6b303b267ea42ed8a6065c5716a2e26
Ricardo <up201202409@fc.up.pt>
2014-11-20 16:48:23 +0000

    Split libraries/source code/make files a bit, put source code into src/ folder

--- make.sh
+++ make.sh
@@ -4,25 +4,25 @@
 
 set +e # stop on error
 
-cd flex
+cd src/flex/
 flex lexer # creatures lex.yy.c
 
-cd ..
+cd ../../
 
 mkdir -p build # gcc won't build the directory
 
 if [ -n "$1" ]; then
 	echo "Building lexer only..."
-	gcc -Wall flex/lex.yy.c -o build/compiler
+	gcc -Wall src/flex/lex.yy.c -o build/compiler
 	exit 0
 fi
 
 
 # creates bison/parser.tab.c and a few other files
 # --report=state creates an .output file for debugging
-cd bison
+cd src/bison/
 bison -d parser.y --report=state
 
-cd ..
-gcc -Wall flex/lex.yy.c bison/parser.tab.c libs/libyywrap.c -o build/compiler
+cd ../../
+gcc -Wall src/flex/lex.yy.c src/bison/parser.tab.c libs/libyywrap.c -o build/compiler
 

22ba42ac652bbe6697ef711e50f76f438a9912e4
Ricardo <up201202409@fc.up.pt>
2014-11-27 11:46:10 +0000

    Flex isn't -Wall safe, so remove -Wall. Improve make file output

--- make.sh
+++ make.sh
@@ -4,25 +4,30 @@
 
 set +e # stop on error
 
+# color constants
+LC='\033[0;33m' # log color - dark grey
+NC='\033[0m' # no color
+
 cd src/flex/
+
+echo -e "${LC}-- Flex Start --${NC}"
 flex lexer # creatures lex.yy.c
+echo -e "${LC}-- Flex End --${NC}"
 
 cd ../../
 
 mkdir -p build # gcc won't build the directory
 
-if [ -n "$1" ]; then
-	echo "Building lexer only..."
-	gcc -Wall src/flex/lex.yy.c -o build/compiler
-	exit 0
-fi
-
-
 # creates bison/parser.tab.c and a few other files
 # --report=state creates an .output file for debugging
 cd src/bison/
+echo -e "${LC}-- Bison Start --${NC}"
 bison -d parser.y --report=state
+echo -e "${LC}-- Bison End --${NC}"
 
 cd ../../
-gcc -Wall src/flex/lex.yy.c src/bison/parser.tab.c libs/libyywrap.c src/symbol_table.c src/parse_node.c -o build/compiler
+echo -e "${LC}-- GCC Start --${NC}"
+gcc src/flex/lex.yy.c src/bison/parser.tab.c libs/libyywrap.c src/symbol_table.c src/parse_node.c -o build/compiler
+echo -e "${LC}-- GCC End --${NC}"
+
 

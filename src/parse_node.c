a97b942961c2e33785ea8ffd7b971e6b5ab7fd43
Ricardo <up201202409@fc.up.pt>
2014-11-27 15:36:23 +0000

    Constructors/destructors for parse_node

--- src/parse_node.c
+++ src/parse_node.c
@@ -1,6 +1,30 @@
 
+#include <stdlib.h>
 #include "parse_node.h"
 
 // global variable
 parse_node* root = (parse_node*)0;
 
+parse_node* alloc_parse_node()
+{
+    parse_node* node = (parse_node*)malloc(sizeof(parse_node));
+    node->type = 0;
+    node->left = NULL;
+    node->right = NULL;
+    node->next = NULL;
+    return node;
+}
+
+void free_parse_node(parse_node* node)
+{
+    if (node == NULL)
+        return;
+
+    parse_node* left = node->left;
+    parse_node* right = node->right;
+    parse_node* next = node->next;
+    free(node);
+    free_parse_node(left);
+    free_parse_node(right);
+    free_parse_node(next);
+}
--- src/parse_node.h
+++ src/parse_node.h
@@ -6,8 +6,15 @@ struct parse_node
     
     struct parse_node* left;
     struct parse_node* right;
+    
+    struct parse_node* next; // for list constructs
 };
 
 typedef struct parse_node parse_node;
 
+/* functions involving parse_node */
+parse_node* alloc_parse_node();
+void free_parse_node(parse_node* node);
+
+/* global variables */
 extern parse_node* root;
